9e1898bc6d8eca349b10e99c620682b5125dbe4e
mdmonir-hossain <monirsoftengr@gmail.com>
2023-12-06 01:00:46 +0600

    add to favourite

--- src/Pages/DashBoard/DashboardPages/AddTOFavourite.jsx
+++ src/Pages/DashBoard/DashboardPages/AddTOFavourite.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-const AddTOFavourite = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AddTOFavourite = ({ addfavourite }) => {
+    const {
+      _id,
+      BiodataId,
+      BiodataType,
+      Name,
+      ProfileImageLink,
+      DateOfBirth,
+      Height,
+      Weight,
+      Age,
+      Occupation,
+      Race,
+      FathersName,
+      MothersName,
+      PermanentDivisionName,
+      PresentDivisionName,
+      ExpectedPartnerAge,
+      ExpectedPartnerHeight,
+      ExpectedPartnerWeight,
+      ContactEmail,
+      MobileNumber,
+      AccountType,
+      MarriageCompleted,
+      Reviews,
+    } = addfavourite;
+    return <div>{BiodataId}</div>;
 };
 
 export default AddTOFavourite;
