31536d3e14558f32c1a42c05c2aa05c042fc9e35
mdmonir-hossain <monirsoftengr@gmail.com>
2023-12-06 00:47:52 +0600

    biodata Filter

--- src/Pages/AllbioDatas.jsx/AllbioDatas.jsx
+++ src/Pages/AllbioDatas.jsx/AllbioDatas.jsx
@@ -5,32 +5,38 @@ import Biodatas from "../Biodatas/Biodatas";
 
 const AllbioDatas = () => {
   const [asc, setAsc] = useState(true);
-  const [type, setType] = useState(true);
+  const [Male, setMale] = useState(true);
+  const [selectedDivision, setSelectedDivision] = useState('');
   const [allbiodatas, setAllbiodatas] = useState([]);
-  const { isPending, data,refetch  } = useQuery({
-    queryKey: ["allbiodatas"],
-    queryFn: async () => {
-      const res = await fetch(
-        `https://b8a12-server-side-six.vercel.app/allbiodatas?sort=${asc ? 'asc' : 'desc'}`
-      );
-      return res.json();
-  
-    },
-  });
-
-  if (isPending) {
-    return <Spinner aria-label="Default status example" />;
+  const handleDivisionChange = (event) => {
+    setSelectedDivision(event.target.value);
   }
 
-  // useEffect(() => {
-  //   fetch(
-  //     `https://b8a12-server-side-six.vercel.app/allbiodatas?sort=${
-  //       asc ? "asc" : "desc"
-  //     }`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => setAllbiodatas(data));
-  // }, [asc]);
+  console.log(selectedDivision);
+  // const { isPending, data,refetch  } = useQuery({
+  //   queryKey: ["allbiodatas"],
+  //   queryFn: async () => {
+  //     const res = await fetch(
+  //       `https://b8a12-server-side-six.vercel.app/allbiodatas?sort=${asc ? 'asc' : 'desc'}`
+  //     );
+  //     return res.json();
+  
+  //   },
+  // });
+
+  // if (isPending) {
+  //   return <Spinner aria-label="Default status example" />;
+  // }
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:5000/allbiodatas?sort=${asc ? "asc" : "desc"}&gender=${
+        Male ? "Male" : "Female"
+      }&division=${selectedDivision}`
+    )
+      .then((res) => res.json())
+      .then((data) => setAllbiodatas(data));
+  }, [asc, Male, selectedDivision]);
   return (
     <div className="flex">
       <div className="w-full  lg:w-[200px] bg-[#522b79] min-h-screen">
@@ -39,11 +45,10 @@ const AllbioDatas = () => {
         <hr />
         <div className="flex flex-col gap-3">
           <Button onClick={() => setAsc(!asc)}>
-            {" "}
             {asc ? "Age: High to Low " : "Age: Low to High"}{" "}
           </Button>
-          <Button onClick={() => setType(!type)}>
-            {type ? "Male " : "Female"}{" "}
+          <Button onClick={() => setMale(!Male)}>
+            {Male ? "Male " : "Female"}{" "}
           </Button>
           <div className="max-w-md">
             <div className="mb-2 block text-white">
@@ -53,20 +58,21 @@ const AllbioDatas = () => {
                 value="Filter by division "
               />
             </div>
-            <Select id="division" required>
-              <option>Dhaka</option>
-              <option>Chattogram</option>
-              <option>Rangpur</option>
-              <option>Barisal</option>
-              <option>Khulna</option>
-              <option>Maymansign</option>
-              <option>Sylhet</option>
+            <Select id="division" onChange={handleDivisionChange} required>
+              <option value="">ALL</option>
+              <option value="Dhaka">Dhaka</option>
+              <option value="Chattogram">Chattogram</option>
+              <option value="Rangpur">Rangpur</option>
+              <option value="Barisal">Barisal</option>
+              <option value="Khulna">Khulna</option>
+              <option value="Maymansign">Maymansign</option>
+              <option value="Sylhet">Sylhet</option>
             </Select>
           </div>
         </div>
       </div>
       <div className="grid grid-cols-1   lg:grid-cols-3 md:grid-cols-2 mt-10 gap-5">
-        {data?.map((allbio) => (
+        {allbiodatas?.map((allbio) => (
           <Biodatas allbio={allbio}></Biodatas>
         ))}
       </div>
