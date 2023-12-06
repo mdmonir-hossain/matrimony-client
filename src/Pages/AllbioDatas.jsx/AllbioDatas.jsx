f964c6aad4da7f1c64e2587b865fd60610b4eab9
mdmonir-hossain <monirsoftengr@gmail.com>
2023-12-06 12:10:33 +0600

    pagination

--- src/Pages/AllbioDatas.jsx/AllbioDatas.jsx
+++ src/Pages/AllbioDatas.jsx/AllbioDatas.jsx
@@ -8,6 +8,7 @@ const AllbioDatas = () => {
   const [Male, setMale] = useState(true);
   const [selectedDivision, setSelectedDivision] = useState('');
   const [allbiodatas, setAllbiodatas] = useState([]);
+  const [page,setPage]=useState(0)
   const handleDivisionChange = (event) => {
     setSelectedDivision(event.target.value);
   }
@@ -32,49 +33,62 @@ const AllbioDatas = () => {
     fetch(
       `https://b8a12-server-side-six.vercel.app/allbiodatas?sort=${
         asc ? "asc" : "desc"
-      }&gender=${Male ? "Male" : "Female"}&division=${selectedDivision}`
+      }&gender=${
+        Male ? "Male" : "Female"
+      }&division=${selectedDivision}&page=${page}`
     )
       .then((res) => res.json())
-      .then((data) => setAllbiodatas(data));
-  }, [asc, Male, selectedDivision]);
+      .then((data) => setAllbiodatas(data.result));
+  }, [asc, Male, selectedDivision, page]);
   return (
-    <div className="flex">
-      <div className="w-full  lg:w-[200px] bg-[#522b79] min-h-screen">
-        <h1 className="text-white text-center text-3xl"> Filter</h1>
+    <div>
+      <div className="flex">
+        <div className="w-full  lg:w-[200px] bg-[#522b79] min-h-screen">
+          <h1 className="text-white text-center text-3xl"> Filter</h1>
 
-        <hr />
-        <div className="flex flex-col gap-3">
-          <Button onClick={() => setAsc(!asc)}>
-            {asc ? "Age: High to Low " : "Age: Low to High"}{" "}
-          </Button>
-          <Button onClick={() => setMale(!Male)}>
-            {Male ? "Male " : "Female"}{" "}
-          </Button>
-          <div className="max-w-md">
-            <div className="mb-2 block text-white">
-              <Label
-                htmlFor="division"
-                className="text-white"
-                value="Filter by division "
-              />
+          <hr />
+          <div className="flex flex-col gap-3">
+            <Button onClick={() => setAsc(!asc)}>
+              {asc ? "Age: High to Low " : "Age: Low to High"}{" "}
+            </Button>
+            <Button onClick={() => setMale(!Male)}>
+              {Male ? "Male " : "Female"}
+            </Button>
+            <div className="max-w-md">
+              <div className="mb-2 block text-white">
+                <Label
+                  htmlFor="division"
+                  className="text-white"
+                  value="Filter by division "
+                />
+              </div>
+              <Select id="division" onChange={handleDivisionChange} required>
+                <option value="">ALL</option>
+                <option value="Dhaka">Dhaka</option>
+                <option value="Chattogram">Chattogram</option>
+                <option value="Rangpur">Rangpur</option>
+                <option value="Barisal">Barisal</option>
+                <option value="Khulna">Khulna</option>
+                <option value="Maymansign">Maymansign</option>
+                <option value="Sylhet">Sylhet</option>
+              </Select>
             </div>
-            <Select id="division" onChange={handleDivisionChange} required>
-              <option value="">ALL</option>
-              <option value="Dhaka">Dhaka</option>
-              <option value="Chattogram">Chattogram</option>
-              <option value="Rangpur">Rangpur</option>
-              <option value="Barisal">Barisal</option>
-              <option value="Khulna">Khulna</option>
-              <option value="Maymansign">Maymansign</option>
-              <option value="Sylhet">Sylhet</option>
-            </Select>
           </div>
         </div>
+        <div className="grid grid-cols-1   lg:grid-cols-3 md:grid-cols-2 mt-10 gap-5">
+          {allbiodatas?.map((allbio) => (
+            <Biodatas allbio={allbio}></Biodatas>
+          ))}
+        </div>
       </div>
-      <div className="grid grid-cols-1   lg:grid-cols-3 md:grid-cols-2 mt-10 gap-5">
-        {allbiodatas?.map((allbio) => (
-          <Biodatas allbio={allbio}></Biodatas>
-        ))}
+      <div className="flex justify-center items-center gap-2">
+        <Button className="bg-[#e92f83]" onClick={() => setPage(page - 1)}>
+          Prevous Page
+        </Button>
+        <Button className="bg-[#e92f83]" onClick={() => setPage(page + 1)}>
+          {" "}
+          Next Page
+        </Button>
       </div>
     </div>
   );
